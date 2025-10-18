91ee8e82f3b874632d934098f986172436387018
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2025-10-18 00:31:02 -0400

    Harden serializers: guard missing groups, robust parsing of rankings/colors; avoid errors in topic views

--- plugin.rb
+++ plugin.rb
@@ -14,28 +14,64 @@ after_initialize do
   
   # Add user groups to serialized user data
   add_to_serializer(:basic_user, :user_groups) do
-    object.groups.pluck(:name).map(&:downcase)
+    groups =
+      if object.respond_to?(:groups)
+        object.groups.pluck(:name)
+      else
+        []
+      end
+    groups.map { |n| n.to_s.downcase }
   end
 
   # Also add to full User serializer (used by /u/:username.json)
   add_to_serializer(:user, :user_groups) do
-    object.groups.pluck(:name).map(&:downcase)
+    groups =
+      if object.respond_to?(:groups)
+        object.groups.pluck(:name)
+      else
+        []
+      end
+    groups.map { |n| n.to_s.downcase }
   end
 
 
   # Add highest ranked group for coloring
   add_to_serializer(:basic_user, :highest_ranked_group) do
-    groups = object.groups.pluck(:name).map(&:downcase)
-    rankings = SiteSetting.discoursecord_group_rankings || []
-    
-    groups.sort_by { |group| rankings.index(group) || Float::INFINITY }.first
+    groups =
+      if object.respond_to?(:groups)
+        object.groups.pluck(:name).map { |n| n.to_s.downcase }
+      else
+        []
+      end
+
+    rankings_raw = SiteSetting.discoursecord_group_rankings
+    rankings =
+      if rankings_raw.is_a?(Array)
+        rankings_raw.map { |r| r.to_s.downcase }
+      else
+        rankings_raw.to_s.split(",").map { |r| r.strip.downcase }
+      end
+
+    groups.min_by { |g| (idx = rankings.index(g)) ? idx : Float::INFINITY }
   end
 
   add_to_serializer(:user, :highest_ranked_group) do
-    groups = object.groups.pluck(:name).map(&:downcase)
-    rankings = SiteSetting.discoursecord_group_rankings || []
+    groups =
+      if object.respond_to?(:groups)
+        object.groups.pluck(:name).map { |n| n.to_s.downcase }
+      else
+        []
+      end
 
-    groups.sort_by { |group| rankings.index(group) || Float::INFINITY }.first
+    rankings_raw = SiteSetting.discoursecord_group_rankings
+    rankings =
+      if rankings_raw.is_a?(Array)
+        rankings_raw.map { |r| r.to_s.downcase }
+      else
+        rankings_raw.to_s.split(",").map { |r| r.strip.downcase }
+      end
+
+    groups.min_by { |g| (idx = rankings.index(g)) ? idx : Float::INFINITY }
   end
 
 
@@ -43,8 +79,18 @@ after_initialize do
   add_to_serializer(:basic_user, :group_color) do
     highest_group = highest_ranked_group
     if highest_group
-      colors = SiteSetting.discoursecord_group_colors || {}
-      colors[highest_group] || '#000000'
+      colors_raw = SiteSetting.discoursecord_group_colors
+      colors =
+        if colors_raw.is_a?(Hash)
+          colors_raw
+        else
+          begin
+            JSON.parse(colors_raw)
+          rescue
+            {}
+          end
+        end
+      colors[highest_group] || "#000000"
     else
       nil
     end
@@ -53,8 +99,18 @@ after_initialize do
   add_to_serializer(:user, :group_color) do
     highest_group = highest_ranked_group
     if highest_group
-      colors = SiteSetting.discoursecord_group_colors || {}
-      colors[highest_group] || '#000000'
+      colors_raw = SiteSetting.discoursecord_group_colors
+      colors =
+        if colors_raw.is_a?(Hash)
+          colors_raw
+        else
+          begin
+            JSON.parse(colors_raw)
+          rescue
+            {}
+          end
+        end
+      colors[highest_group] || "#000000"
     else
       nil
     end
